9ba71d7b73183e42717723bcc3cec4184422dc83
Zsolt Ebel <zsolt_ebel@epam.com>
2022-05-21 05:12:54 +0200

    Add initial Spring Boot project

---
+++ src/test/java/com/epam/bitcoing/IntegrationTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.bitcoing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationTestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
