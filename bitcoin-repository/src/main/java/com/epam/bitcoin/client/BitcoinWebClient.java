0d3931e2a0c840b5c526a6a1b4bc516f0aa6632d
Zsolt Ebel <zsolt_ebel@epam.com>
2022-05-27 07:43:18 +0200

    Update exception handling to catch WebClientException

--- bitcoin-repository/src/main/java/com/epam/bitcoin/client/BitcoinWebClient.java
+++ bitcoin-repository/src/main/java/com/epam/bitcoin/client/BitcoinWebClient.java
@@ -5,12 +5,14 @@ import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientException;
 
 import com.epam.bitcoin.domain.CoinDeskResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import reactor.core.publisher.Mono;
 
 public class BitcoinWebClient implements BitcoinClient {
 
@@ -39,7 +41,7 @@ public class BitcoinWebClient implements BitcoinClient {
                     .bodyToMono(CoinDeskResponse.class)
                     .block();
             LOGGER.info("<== The following response was returned: " + objectMapper.writeValueAsString(coinDeskResponse));
-        } catch (RestClientException exception) {
+        } catch (WebClientException exception) {
             LOGGER.error("The following error occurred during CoinDesk call: " + exception.getMessage());
         } catch (JsonProcessingException exception) {
             LOGGER.error("An error occurred during payload processing: " + exception);
