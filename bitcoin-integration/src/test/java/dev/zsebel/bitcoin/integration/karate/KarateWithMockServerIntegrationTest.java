a0f497b4a635da6f3eeba81a6179459c2e879a89
Zsolt Ebel <ebelzsolt@gmail.com>
2025-05-17 11:55:38 +0200

    Remove unused import

--- bitcoin-integration/src/test/java/dev/zsebel/bitcoin/integration/karate/KarateWithMockServerIntegrationTest.java
+++ bitcoin-integration/src/test/java/dev/zsebel/bitcoin/integration/karate/KarateWithMockServerIntegrationTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 /**
